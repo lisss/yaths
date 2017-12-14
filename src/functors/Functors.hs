08d9d943ac188d798bf0cd78f16e1f6ad88756b5
Liss <alicedeadbride@gmail.com>
2017-12-14 23:27:21 +0200

    Functors excersises

--- src/functors/Functors.hs
+++ src/functors/Functors.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Functors where
 
 import Test.QuickCheck
 import Test.QuickCheck.Function
+import GHC.Arr
 
 replaceWithP :: b -> Char
 replaceWithP = const 'p'
@@ -183,6 +185,161 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (Four' a b) where
 -- 8. Can you implement one for this type? Why? Why not?
 -- --> NOPE, because its constructor is a constant
 data Trivial = Trivial
+
+-- Exercise: Possibly
+data Possibly a =
+  LolNope
+  | Yeppers a
+  deriving (Eq, Show)
+
+instance Functor Possibly where
+  fmap _ LolNope = LolNope
+  fmap f (Yeppers a) = Yeppers (f a)
+
+-- Short Exercise
+data Sum a b =
+  First a
+  | Second b
+  deriving (Eq, Show)
+
+instance Functor (Sum a) where
+  fmap _ (First a) = First a
+  fmap f (Second b) = Second (f b)
+
+-- Chapter exercises
+-- 1. NOPE
+-- data Bool = False | True
+
+-- 2.
+data BoolAndSomethingElse a = False' a | True' a
+instance Functor BoolAndSomethingElse where
+  fmap f (False' a) = False' (f a)
+  fmap f (True' a) = True' (f a)
+
+-- 3.
+data BoolAndMaybeSomethingElse a = Falsish | Truish a
+instance Functor BoolAndMaybeSomethingElse where
+  fmap _ Falsish = Falsish
+  fmap f (Truish a) = Truish (f a)
+
+-- 4. --> NOPE
+newtype Mu f = InF { outF :: f (Mu f) }
+
+-- 5. --> NOPE
+data D = D (Array Word Word) Int Int
+
+-- Rearrange the arguments 
+-- 1.
+data Sum' b a =
+  First' a
+  | Second' b
+
+instance Functor (Sum' e) where
+  fmap f (First' a) = First' (f a)
+  fmap f (Second' b) = Second' b
+
+-- 2.
+data Company a b c =
+  DeepBlue a b
+  | Something c
+
+instance Functor (Company e e') where
+  fmap f (Something b) = Something (f b)
+  fmap _ (DeepBlue a c) = DeepBlue a c
+
+-- 3.
+data More a b = L b a b | R a b a
+  deriving (Eq, Show)
+
+instance Functor (More x) where
+  fmap f (L a b a') = L (f a) b (f a')
+  fmap f (R b a b') = R b (f a) b'
+
+-- Write Functor instances for the following datatypes.
+-- 1.
+data Quant a b =
+  Finance
+  | Desk a
+  | Bloor b
+
+instance Functor (Quant a) where
+  fmap _ Finance = Finance
+  fmap _ (Desk a) = Desk a
+  fmap f (Bloor a) = Bloor (f a)
+
+-- 2.
+newtype K a b = K a
+
+instance Functor (K a) where
+  fmap f (K a) = K a
+
+-- 3.
+newtype Flip f a b =
+  Flip (f b a)
+  deriving (Eq, Show)
+
+instance Functor (Flip K a) where
+  fmap f (Flip (K a)) = Flip $ K (f a)
+
+-- 4.
+newtype EvilGoateeConst a b = GoatyConst b
+
+instance Functor (EvilGoateeConst a) where
+  fmap f (GoatyConst a) = GoatyConst (f a)
+
+-- 5.
+newtype LiftItOut f a = LiftItOut (f a)
+
+instance Functor f => Functor (LiftItOut f) where
+  fmap f (LiftItOut fa) = LiftItOut (fmap f fa)
+
+-- 6.
+data Parappa f g a = DaWrappa (f a) (g a)
+
+instance (Functor f, Functor g) => Functor (Parappa f g) where
+  fmap f (DaWrappa fa ga) = DaWrappa (fmap f fa) (fmap f ga)
+
+-- 7.
+data IgnoreOne f g a b = IgnoringSomething (f a) (g b)
+
+instance (Functor f, Functor g) => Functor (IgnoreOne f g a) where
+  fmap f (IgnoringSomething fa ga) = IgnoringSomething fa (fmap f ga)
+
+-- 8.
+data Notorious g o a t = Notorious (g o) (g a) (g t)
+
+instance Functor g => Functor (Notorious g o a) where
+  fmap f (Notorious ga go gt) = Notorious ga go (fmap f gt)
+
+-- 9.
+data List a = Nil | Cons a (List a) deriving Show
+instance Functor List where
+  fmap _ Nil = Nil
+  fmap f (Cons a l) = Cons (f a) (fmap f l)
+
+-- 10.
+data GoatLord a =
+  NoGoat
+  | OneGoat a
+  | MoreGoats (GoatLord a)
+  (GoatLord a)
+  (GoatLord a)
+
+-- instance Functor GoatLord where
+--   fmap _ NoGoat = NoGoat
+--   fmap f (OneGoat a) = OneGoat (f a)
+--   fmap f (MoreGoats a b c) = MoreGoats (GoatLord a) (GoatLord b) (GoatLord c)
+
+-- 11. does it work tho ???
+data TalkToMe a =
+  Halt
+  | Print String a
+  | Read (String -> a)
+
+instance Functor TalkToMe where
+  fmap _ Halt = Halt
+  fmap f (Print a b) = Print a (f b)
+  fmap f (Read f') = Read (fmap f f')
 
 --- MAIN ---
 main :: IO ()
