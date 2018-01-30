d93ed02d618c09b49ddf6e3f0f36477b1a9a2abe
Liss <alicedeadbride@gmail.com>
2018-01-30 15:53:31 +0200

    A bit of Reader

---
+++ src/reader_state/Reader.hs
@@ -0,0 +1,163 @@
+{-# LANGUAGE InstanceSigs #-}
+module Reader where
+
+import Data.Char
+import Control.Monad(join)
+import Control.Applicative
+import Data.Maybe
+
+-- Short Exercise: Warming Up
+cap :: [Char] -> [Char]
+cap xs = map toUpper xs
+
+rev :: [Char] -> [Char]
+rev xs = reverse xs
+
+composed :: [Char] -> [Char]
+composed = rev . cap
+
+fmapped :: [Char] -> [Char]
+fmapped = rev <$> cap
+
+tupled :: [Char] -> ([Char], [Char])
+tupled = (,) <$> cap <*> rev
+
+tupled' :: [Char] -> ([Char], [Char])
+tupled' = do
+  a <- cap
+  b <- rev
+  return (a, b)
+
+tupled'' :: [Char] -> ([Char], [Char])
+tupled'' = cap >>= \a' -> rev >>= \b' -> return (a', b')
+
+-- Exercise: Ask
+newtype Reader r a = Reader { runReader :: r -> a }
+
+instance Functor (Reader r) where
+  fmap :: (a -> b) -> Reader r a -> Reader r b
+  fmap f (Reader ra) = Reader (f . ra)
+
+ask :: Reader a a
+ask = Reader id
+
+-- Exercise: Reading Comprehension
+-- 1.
+myLiftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
+myLiftA2 f x y = f <$> x <*> y
+
+-- 2.
+asks :: (r -> a) -> Reader r a
+asks = Reader
+
+-- 3.
+instance Applicative (Reader r) where
+  pure :: a -> Reader r a
+  pure a = Reader $ \_ -> a
+  (<*>) :: Reader r (a -> b) -> Reader r a -> Reader r b
+  (<*>) (Reader rab) (Reader ra) = Reader $ \r -> rab r . ra $ r
+
+-- Exercise: Reader Monad
+-- 1.
+instance Monad (Reader r) where
+  return = pure
+  (>>=) :: Reader r a -> (a -> Reader r b) -> Reader r b
+  (>>=) (Reader ra) f = Reader $ \r -> runReader (f . ra $ r) r
+
+-- 2.
+newtype HumanName =
+  HumanName String
+  deriving (Eq, Show)
+
+newtype DogName =
+  DogName String
+  deriving (Eq, Show)
+
+newtype Address =
+  Address String
+  deriving (Eq, Show)
+
+data Person =
+  Person {
+  humanName :: HumanName
+  , dogName :: DogName
+  , address :: Address
+  } deriving (Eq, Show)
+
+data Dog =
+  Dog {
+  dogsName :: DogName
+  , dogsAddress :: Address
+  } deriving (Eq, Show)
+
+getDogRM :: Person -> Dog
+getDogRM = do
+  name <- dogName
+  addy <- address
+  return $ Dog name addy
+
+getDogRM' :: Reader Person Dog
+getDogRM' = Dog <$> Reader dogName <*> Reader address
+
+pers :: Person
+pers =
+  Person (HumanName "Big Bird")
+  (DogName "Barkley")
+  (Address "Sesame Street")
+  
+-- Chapter Exercises
+
+x = [1, 2, 3]
+y = [4, 5, 6]
+z = [7, 8, 9]
+
+xs :: Maybe Integer
+xs = lookup 3 $ zip x y
+
+ys :: Maybe Integer
+ys = lookup 6 $ zip y z
+
+zs :: Maybe Integer
+zs = lookup 4 $ zip x y
+
+z' :: Integer -> Maybe Integer
+z' n = lookup n $ zip x z
+
+x1 :: Maybe (Integer, Integer)
+x1 = (,) <$> xs <*> ys
+
+x2 :: Maybe (Integer, Integer)
+x2 = (,) <$> ys <*> zs
+
+x3 :: Integer -> (Maybe Integer, Maybe Integer)
+x3 n = (,) (z' n) (z' n)
+
+summed :: Num c => (c, c) -> c
+summed = uncurry (+)
+
+bolt :: Integer -> Bool
+-- use &&, >3, <8
+bolt = (&&) <$> (>3) <*> (<8)
+
+sequA :: Integral a => a -> [Bool]
+sequA m = sequenceA [(>3), (<8), even] m
+
+s' = summed <$> ((,) <$> xs <*> ys)
+
+--- MAIN ---
+main :: IO ()
+main = do
+  print $ runReader getDogRM' pers
+  print ">>> Chapter Exercises"
+  print $ sequenceA [Just 3, Just 2, Just 1]
+  print $ sequenceA [x, y]
+  print $ sequenceA [xs, ys]
+  print $ summed <$> ((,) <$> xs <*> ys)
+  print $ fmap summed ((,) <$> xs <*> zs)
+  print $ bolt 7
+  print $ fmap bolt z
+  print $ sequenceA [(>3), (<8), even] 7
+  ----
+  print $ foldr (&&) True seqA 30
+  sequA $ fromMaybe 0 s'
+  bolt $ fromMaybe 0 ys
